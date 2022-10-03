936377bdf92d429b1c5ff8f5ab3e339e6df092fc
Robson <robson.miranda@lunes.io>
2022-10-03 07:55:26 -0300

    refactor: prettier configutations

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,35 @@
-import React from "react";
-import { ethers } from "ethers";
+import React from "react"
 
-import useEthereum from "./hooks/useEthereum";
+import useEthereum from "./hooks/useEthereum"
 
-import "./App.css";
+import "./App.css"
 
 export default function App() {
-  const { account, connectWallet } = useEthereum();
+    const { account, connectWallet } = useEthereum()
 
-  const wave = () => {};
+    return (
+        <div className="mainContainer">
+            <div className="dataContainer">
+                <div className="header">👋 Olá Pessoal!</div>
 
-  return (
-    <div className="mainContainer">
-      <div className="dataContainer">
-        <div className="header">👋 Olá Pessoal!</div>
+                <div className="bio">
+                    Eu sou o Robson e só estou tentando ganhar um pouco mais pra
+                    sair do aperto. Legal, né? Conecte sua carteira Ethereum
+                    wallet e me manda um tchauzinho!
+                </div>
 
-        <div className="bio">
-          Eu sou o Robson e só estou tentando ganhar um pouco mais pra sair do
-          aperto. Legal, né? Conecte sua carteira Ethereum wallet e me manda um
-          tchauzinho!
+                <button className="waveButton" onClick={wave}>
+                    Mandar Tchauzinho 👋
+                </button>
+
+                <button
+                    className="waveButton"
+                    disabled={account.length > 0}
+                    onClick={connectWallet}
+                >
+                    Conectar carteira
+                </button>
+            </div>
         </div>
-
-        <button className="waveButton" onClick={wave}>
-          Mandar Tchauzinho 👋
-        </button>
-
-        <button
-          className="waveButton"
-          disabled={account.length > 0}
-          onClick={connectWallet}
-        >
-          Conectar carteira
-        </button>
-      </div>
-    </div>
-  );
+    )
 }
